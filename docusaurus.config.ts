f0c741b3387b6caab08c736e4db96c02017fcab2
Marco Giuliotti <marco.giuliotti@bitbull.it>
2025-11-12 12:49:01 +0100

    update branch name

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -24,7 +24,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'MarcoJul', // Usually your GitHub org/user name.
   projectName: 'reg', // Usually your repo name.
-  deploymentBranch: 'main', // The branch GitHub Pages will use
+  deploymentBranch: 'gh-pages', // The branch GitHub Pages will use
   trailingSlash: false,
   onBrokenLinks: 'throw',
 
