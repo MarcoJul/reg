a8bf22902300552a2f16ed522c44fc93766b6415
Marco Giuliotti <marco.giuliotti@bitbull.it>
2025-11-12 12:42:09 +0100

    Configure Github Pages Deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,16 +15,17 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://MarcoJul.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/reg/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'MarcoJul', // Usually your GitHub org/user name.
+  projectName: 'reg', // Usually your repo name.
+  deploymentBranch: 'main', // The branch GitHub Pages will use
+  trailingSlash: false,
   onBrokenLinks: 'throw',
 
   // Even if you don't use internationalization, you can use this field to set
